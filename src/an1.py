20ed2a92d2f27dd840d8c611c395617a26a9873a
Mike Gleen <mike.gleen@gmail.com>
2014-12-28 12:00:08 -0600

    Applied dropna().

--- src/an1.py
+++ src/an1.py
@@ -3,7 +3,7 @@ import argparse
 import pandas as pd
 __author__ = 'mlg'
 
-GRADEMAP = {'E': 1, 'D': 2, 'C': 3, 'B': 4, 'A': 5, 'A*': 6}
+GRADE = pd.Series({'E': 1, 'D': 2, 'C': 3, 'B': 4, 'A': 5, 'A*': 6})
 
 
 def getparser():
@@ -23,6 +23,9 @@ def getargs():
     return args
 
 
+def grade(g):
+    return GRADE[g[0]]
+
 def main(args):
     df = pd.read_csv(args.targetfile)
     df = df[['name', 'y11_ap5', '3lop_3', 'fftd_2']]
@@ -39,7 +42,12 @@ def main(args):
     mf.rename(columns={'y11_ap5': 'ap5', '3lop_3': '3lop', 'fftd_2': 'fftd',
                        'result_2': 'result'}, inplace=True)
     print(mf)
-
+    mf.dropna(inplace=True)
+    ap5_g = mf['ap5'].map(grade)
+    lop_g = mf['3lop'].map(grade)
+    fftd_g = mf['fftd'].map(grade)
+    result_g = mf['result'].map(grade)
+    print(pd.DataFrame([mf.name, ap5_g, lop_g, fftd_g, result_g]).T)
 if __name__ == '__main__':
     _args = getargs()
     main(_args)
